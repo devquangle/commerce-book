0acad74dbb05766da5523a8429dcd1cad39a0695
devquangle <devquangle8386@gmail.com>
2026-08-02 22:35:10 +0700

    feat: Remove unused CookieValue import from AuthController

--- backend/src/main/java/com/dev/backend/modules/auth/controller/AuthController.java
+++ backend/src/main/java/com/dev/backend/modules/auth/controller/AuthController.java
@@ -19,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
